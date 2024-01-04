055d37d2bbcaf56bdfa466d219ce9fb9cc19cc77
c-l-scholl <cscholl@macalester.edu>
2024-01-04 15:10:22 -0800

    unpack ipv4 packet

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -33,8 +33,24 @@ def ethernet_frame(data):
 # return properly formatted MAC address (AA:BB:CC:DD:EE:FF)
 
 def get_mac_addr(bytes_addr):
-	bytes_string = map('{:02x}'.format, bytes_addr)
-	return ':'.join(bytes_string).upper()
+	bytes_str = map('{:02x}'.format, bytes_addr)
+	return ':'.join(bytes_str).upper()
+
+# unpack IPv4 packet
+def ipv4_packet(data):
+    # header 
+	version_header_length = data[0]
+	version = version_header_length >> 4
+	header_length = (version_header_length & 15) * 4
+
+	# other information
+	ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+	return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[version_header_length:]
+
+# Returns prperly formatted IPv4 address
+def ipv4(addr):
+	return '.'.join(map(str, addr))
+
 
 if __name__ == "__main__":
 	main() 
