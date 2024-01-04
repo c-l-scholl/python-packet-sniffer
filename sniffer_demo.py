ca9a6d5888c2f0c8f6027294695a967c2867cd4c
c-l-scholl <cscholl@macalester.edu>
2024-01-03 17:17:46 -0800

    deleted unnecessary stuff

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -4,19 +4,3 @@ from textwrap import *
 
 # unpack ethernet frame
 
-# first learn how to use struct (data to bytes)
-# serialization
-packed_data = pack('iif', 6, 19, 4.23)
-#					iif means integer, integer, float
-#					followed by comma separated values
-print(packed_data)
-print(calcsize('i'))
-print(calcsize('f'))
-print(calcsize('iif'))
-
-# to return from byte to data
-unpacked_data = unpack('iif', packed_data)
-print(unpacked_data)
-
-print(unpack('iif', b'\x06\x00\x00\x00\x13\x00\x00\x00)\\\x87@'))
-# need leading b (not in quotes)
