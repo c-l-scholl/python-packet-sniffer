9130ada144a62633c967f61c001bf2c7c9a437c9
c-l-scholl <cscholl@macalester.edu>
2024-01-05 11:52:37 -0800

    added imcp, tcp, and udp unpack methods

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -51,6 +51,29 @@ def ipv4_packet(data):
 def ipv4(addr):
 	return '.'.join(map(str, addr))
 
+# Unpacks ICMP Packet 
+def icmp_packet(data):
+	icmp_type, code, checksum = struct.unpack('! B B H', data[4:])
+	return icmp_type, code, checksum 
+
+# Unpack TCP Packet
+def tcp_packet(data):	
+	src_port, desc_port, sequence, acknowledgement, offset_reserved_flags = struct.unpack('! H H L L H', data[14:])
+	offset = (offset_reserved_flags >> 12) * 4
+	flag_urg = (offset_reserved_flags >> 32) * 5
+	flag_ack = (offset_reserved_flags >> 16) * 4
+	flag_psh = (offset_reserved_flags >> 8) * 3
+	flag_rst = (offset_reserved_flags >> 4) * 2
+	flag_syn = (offset_reserved_flags >> 2) * 1
+	flag_fin = (offset_reserved_flags >> 1)
+	return src_port, desc_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+
+# Unpack udp packet (optional)
+def udp_packet(data):
+	src_port, desc_port, size = struct.unpack('! H H 2x H', data[:8])
+	return src_port, desc_port, size, data[8:]
+
+
 
 if __name__ == "__main__":
 	main() 
